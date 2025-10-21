94c33de49db04e8433f0383dd4aa17cdf8880984
unknown <diego.macia-d@labinfo.is.escuelaing.edu.co>
2025-10-21 08:58:27 -0500

    first commit

---
+++ proxy-server/src/main/java/org/eci/arep/math_service/MathServiceApplication.java
@@ -0,0 +1,13 @@
+package org.eci.arep.math_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathServiceApplication.class, args);
+	}
+
+}
